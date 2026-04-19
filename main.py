5fece5fb8de8aed57e0553bfade498f8410a0e7c
beppo <beppo@mail.com>
2026-04-19 19:31:27 +0300

    refresh new main file

--- main.py
+++ main.py
@@ -1,93 +1,80 @@
-"""
-Compare `is` and `==`
-"""
+import time
+import functools
 
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = a
+def trace (func):
+    trace.depth = 0
 
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        indent = " " * trace.depth
+        print(f"{indent}--> Вход в {func.__name__}({args[0]}")
 
-print("id of a:", id(a))
-print("id of b:", id(b))
-print("id of c:", id(c))
+        trace.depth +- 1
+        result = func(*args, **kwargs)
+        trace.depth -= 1
 
-
-print("a == b:", a == b)
-print("a is b:", a is b)
-
-print("a == c:", a == c)
-print("a is c:", a is c)
-
-num_a = 1
-num_b = 1
-
-print("num_a == num_b", num_a == num_b)
-print("num_a is num_b", num_a is num_b)
-
-print()
-
-print(a, b, c)
-a.append(4)
-b.append(9)
-c.append(5)
-print(a, b, c)
-
-print("a is list type?", isinstance(a, list))
-print("a is list-like type?", isinstance(a, (list, tuple)), type(a))
-
-t = ()
-print("t is list-like type?", isinstance(t, (list, tuple)), type(t))
-
-exists = True
-
-print("type of exists:", type(exists))
-print("'exists' is type of int?", isinstance(exists, int))
-print("exists to int:", int(exists))
-print("exists is 1?", exists is 1)
-print("exists == 1?", exists == 1)
-print("id of exists:", id(exists))
-print("id of int 1:", id(int(1)))
+        print(f"{indent}<-- Выход из {func.__name__}({args[0]}) = {result}")
+        return result
+    return wrapper
 
 
-print("type(a)", type(a))
-print("type(a) is type(b)", type(a) is type(b))
-print("type(a) is type(c)", type(a) is type(c))
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+
+        result = func(*args, **kwargs)
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Функция {func.__name__} выполнилась за {execution_time:.4f} сек.")
+
+        return result
+    return wrapper
+
+@timer_decorator
+def pow (inpute_list):
+    degree = int(input("Выберете и введите число для возведение в выбранную вами степень: "))
+    for item in inpute_list:
+        print(item ** degree)
+        
+
+@timer_decorator
+def even_and_odd (inpute_list):
+    for item in inpute_list:
+        if item % 2 == 0:
+            print(f"{item}, Число четное")
+        else:
+            print(f"{item}, Число нечетное")
+
+@trace
+def fibonachi (num):
+#    if num <= 0: return 0
+#    if num == 1: return 1
+#
+#    prev = 0
+#    now  = 1
+#
+#    for _ in range(num - 1):
+#        next = prev + now
+#        prev = now
+#        now = next
+#
+#    return now
+
+    if num <= 1:
+        return num
+     
+    return fibonachi(num - 1) + fibonachi(num - 2)  
+        
 
 
-def get_list(input_list=None):
-    if input_list is None:
-        input_list = []
+arr1 = [2, 5, 10, 15, 20]
+pow(arr1)
 
-    return input_list
+arr2 = [10, 11, 12, 15, 16, 17, 18, 19, 20]
+even_and_odd(arr2)
 
-
-def get_arr(arr):
-    arr = []
-    return arr
-
-empty_list = []
-res = get_list()
-print("empty list == res?", empty_list == res)
-print("empty list is res?", empty_list is res)
-
-print("empty list is res?", empty_list is get_list(empty_list))
-
-cache = {}
-
-def get_user(user_id):
-    if user_id not in cache:
-        u = ...
-        cache[user_id] = u
-        return u
-    return cache[user_id]
+print(fibonachi(22))
 
 
-u1 = get_user(7)
-u2 = get_user(7)
-"""
-u1 == u2  # is True
 
-u1.id == u2.id  # is True
-
-u1 is not u2  # is True
-"""
